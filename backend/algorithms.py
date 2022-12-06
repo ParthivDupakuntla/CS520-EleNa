6678417d54ac2df2821d432912ce25a31cc3e1dd
Parthiv Dupakuntla <39114608+ParthivDupakuntla@users.noreply.github.com>
2022-12-05 19:00:22 -0500

    update algorithms description

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -1,4 +1,9 @@
 def Astar(source, destination, strategy, pathlimit):
+    """
+    source, destination : nodes for which we derive a path
+    strategy : min/max -> minimum elevation or maximum elevation gain in the path
+    pathlimit : the percentage of path that can be exceeded over the shortest path
+    """
     return "test"
 
 def Dijkstras(source, destination, strategy, pathlimit):
