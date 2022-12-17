d605270f4c5b2aa4c7a4e6427ce1b0df7aa2f0bc
Parthiv Dupakuntla <39114608+ParthivDupakuntla@users.noreply.github.com>
2022-12-16 21:03:32 -0500

    final algo bug fixes

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -26,8 +26,10 @@ class Algorithms:
         self.startNode, self.endNode = None, None
 
 #FINAL SHORTEST PATH 
-    def shortest_path(self, start_location, end_location, pl, algo = "dijkstra", mode = "maximize", log = True):
-        """ Returns the final shortest path route and its corresponding statistics"""
+    def optimalPath(self, start_location, end_location, pl, algo, mode):
+        """ Returns the final shortest path route optimally developed using either astar or dijkstra and its corresponding statistics"""
+        defaultSetting = ["astar", "minimize"] # since we are giving these as the default options in the UI
+        algo, mode = defaultSetting[0], defaultSetting[1]
         G = self.G
         self.pl = pl/100.0
         self.mode = mode
@@ -36,29 +38,20 @@ class Algorithms:
             self.bestPath = [[], 0.0, float('-inf'), float('-inf')]
         else : 
             self.bestPath = [[], 0.0, float('inf'), float('-inf')]
-
         self.startNode, d1 = ox.distance.nearest_nodes(G, start_location[1], start_location[0], return_dist=True)
         self.endNode, d2 = ox.distance.nearest_nodes(G, end_location[1], end_location[0], return_dist=True)
         if d1 > 100 or d2 > 100:
-            if log : print("Nodes are too far away")
-            return None, None
+            return None, None #Nodes are far
         self.shortestRoute = ox.distance.shortest_path(G, orig=self.startNode, dest=self.endNode)
         self.shortestDist  = sum(ox.utils_graph.get_route_edge_attributes(G, self.shortestRoute, 'length'))
-
-
-        if algo == "astar" or mode=="minimize":
-            if log : print("astar")
+        if algo == "astar":
             self.bestPath = Astar(G, self.shortestDist, self.startNode, self.endNode, mode, pl)
-        if log : print("dijkstra")
-
-        self.bestPath =  Dijkstra(G, self.startNode, self.endNode, mode, self.bestPath, pl, self.shortestDist)
+        else:
+            self.bestPath =  Dijkstra(G, self.startNode, self.endNode, mode, self.bestPath, pl, self.shortestDist)
         shortestLatLong = [[G.nodes[route_node]['y'],G.nodes[route_node]['x']] for route_node in self.shortestRoute]
-        
         shortestPathStats = [shortestLatLong, self.shortestDist, \
                             getElevation(G, self.shortestRoute, "gain"), getElevation(G, self.shortestRoute, "drop")]
         if (self.mode == "maximize" and self.bestPath[2] == float('-inf')) or (self.mode == "minimize" and self.bestPath[3] == float('-inf')):            
             return shortestPathStats, [[], 0.0, 0, 0]
         self.bestPath[0] = [[G.nodes[route_node]['y'],G.nodes[route_node]['x']] for route_node in self.bestPath[0]]
-        
-        #print("===>end", self.bestPath[1:])
         return shortestPathStats, self.bestPath
